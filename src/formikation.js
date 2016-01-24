65ce12e1354bd7e9aed160a503ab779b119ccefc
Victor Ortiz <kespers@gmail.com>
2016-01-24 17:14:39 +0100

    Use data-text as file placeholder

--- src/formikation.js
+++ src/formikation.js
@@ -156,17 +156,18 @@ formikation = {
   // Process input file
   processInputFile: function(el) {
     var $div = $('<div class="fk-file-input">'),
-        $el = $(el);
+        $el = $(el),
+        text = $el.attr('data-text');
     $el.on('change', function(e) {
       // e.preventDefault();
       var fn = $el.val().replace(/C:\\fakepath\\/i, '');
       if (!fn) {
-        fn = 'Select file...';
+        fn = text;
       }
       $el.prev('p').html(fn);
     });
     formikation.is_disabled($el, $div);
-    $el.wrap($div).parent().prepend('<p>Select file...</p>');
+    $el.wrap($div.attr('data-text', text)).parent().prepend('<p>' + text + '</p>');
   },
 
   // Updates select width to match span
